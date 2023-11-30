57171c198a91e86b386395155f6c030863229ebc
Trung Nam <trungnam.work@gmail.com>
2023-11-30 16:16:01 +0700

    Xây dựng UI phần Header #3

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
 
+import Button from '~/components/Button';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
@@ -58,8 +59,9 @@ function Header() {
 
                 </div>
             </Tippy>
-            <div className={cx('action')}>
-
+            <div className={cx('actions')}>
+                <Button text>Upload</Button>
+                <Button primary>Log in</Button>
             </div>
         </div>
     </header>
