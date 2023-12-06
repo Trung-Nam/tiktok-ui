19e687de8c6bdd9aceee2bd3b6b9280c7a4cc7ab
Trung Nam <trungnam.work@gmail.com>
2023-12-06 22:18:09 +0700

    Xây dựng UI phần Header #5

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -17,6 +17,21 @@ const MENU_ITEMS = [
     {
         icon: <FontAwesomeIcon icon={faLanguage} />,
         title: 'English',
+        children: {
+            title:'Language',
+            data:[
+                {   
+                    type:'language',
+                    code:'en',
+                    title: 'English'
+                },
+                {
+                    type:'language',
+                    code:'vi',
+                    title: 'Tiếng Việt'
+                }
+            ]
+        }
     },
     {
         icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -38,6 +53,16 @@ function Header() {
             setSearchResult([]);
         }, 0);
     }, []);
+
+    // handle logic
+    const handleMenuChange = (menuItem) => {
+        switch (menuItem.type) {
+            case 'language':
+                // Handle change language
+                break;
+            default:
+        }
+    }
 
     return <header className={cx('wrapper')}>
         <div className={cx('inner')}>
@@ -83,6 +108,7 @@ function Header() {
 
                 <Menu
                     items={MENU_ITEMS}
+                    onChange={handleMenuChange}
                 >
                     <button className={cx('more-btn')}>
                         <FontAwesomeIcon icon={faEllipsisVertical}></FontAwesomeIcon>
--- src/components/Popper/Menu/MenuItem.js
+++ src/components/Popper/Menu/MenuItem.js
@@ -5,10 +5,15 @@ import styles from './Menu.module.scss';
 const cx = classNames.bind(styles);
 
 
-function MenuItem({data}) {
-    return ( 
-        <Button className={cx('menu-item')} leftIcon={data.icon} to={data.to}>
-            {data.title}
+function MenuItem({ data, onClick }) {
+    return (
+        <Button 
+            className={cx('menu-item')} 
+            leftIcon={data.icon} 
+            to={data.to}
+            onClick={onClick}
+        >
+                {data.title}
         </Button>
     );
 }
