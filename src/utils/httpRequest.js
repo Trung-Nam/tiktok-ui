b1863ab78867a0c581252d7ee5b81284805a3d4e
Trung Nam <trungnam.work@gmail.com>
2023-12-15 22:37:48 +0700

    Solve the problem of multiple index.js files

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpRequest = axios.create({
-    baseURL: process.env.REACT_BASE_URL,
+    baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 export const get = async (path, options = {}) => {
